62a7cb19368a2a13157ab36c4a5eb9a8217cbbc4
scott <scott@scopear.com>
2021-12-10 18:58:50 -0800

    Documentation

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # SocketIO-Test
+POC for Unity Socket IO
+
+### Usage: 
+#### Server
+Simple NodeJS server with HTTP backend
+
+`npm install`
+
+`npm start`
+
+#### Unity
+- Add TestObject.cs to a GameObject
+- Connect to port 11102 (defined in app.js)
+
+#### Web
+- Simple web interface to send commands to Unity project
+- Most commands wills simply broadcast to unity project
+- "set value X" will set the value of the cube's Y value
---
+++ Server/httpServer.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const socket = require('socket.io');
+
+var io = socket(server, {
+    pingInterval: 10000,
+    pingTimeout: 5000,
+});
+
+var callbacks = {};
+
+module.exports = {
+    foo: function () {
+        // whatever
+    },
+    bar: function () {
+        // whatever
+    },
+    registerCallback: function(id, cb)
+    {
+        callbacks[id] = cb;
+    },
+    removeCallback: function(id) {
+        delete callbacks[id];
+    }
+
+};
+
+var zemba = function () {
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a web user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', msg => {
+        io.emit('chat message', msg);
+        for(var id in callbacks) {
+            let cb = callbacks[id];
+            cb(msg);
+        }
+    });
+
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
